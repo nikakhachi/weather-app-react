70773334c095f32e46c3bb2f8eb6460cf6f2eb7e
Nika Khachiashvili <o.sicknick@gmail.com>
2021-01-20 22:35:06 +0400

    hiding api token

--- src/Random-Info/randominfo.js
+++ src/Random-Info/randominfo.js
@@ -41,9 +41,9 @@ class RandomInfo extends Component{
     };
 
     async componentDidMount() {
-        const response1 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[0]}&appid=ec6597680d29dc6b93d0586e8e08bc0f&units=metric`);
-        const response2 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[1]}&appid=ec6597680d29dc6b93d0586e8e08bc0f&units=metric`);
-        const response3 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[2]}&appid=ec6597680d29dc6b93d0586e8e08bc0f&units=metric`);
+        const response1 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[0]}&appid=HAHA...NO&units=metric`);
+        const response2 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[1]}&appid=HAHA...NO&units=metric`);
+        const response3 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${randomCities[2]}&appid=HAHA...NO&units=metric`);
         data1 = await response1.json();
         data2 = await response2.json();
         data3= await response3.json();
