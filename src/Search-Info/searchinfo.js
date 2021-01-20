7ec9b5467fd187b20bd17cf3cffade465191e9ff
Nika Khachiashvili <o.sicknick@gmail.com>
2021-01-20 22:35:40 +0400

    Update searchinfo.js

--- src/Search-Info/searchinfo.js
+++ src/Search-Info/searchinfo.js
@@ -22,7 +22,7 @@ function SearchInfo(){
 
     useEffect(() => {
         setWeatherData(undefined);
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ec6597680d29dc6b93d0586e8e08bc0f&units=metric`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=HAHA...NO&units=metric`)
             .then(response => response.json())
             .then(data => {
                 switch(data.weather[0].icon){
